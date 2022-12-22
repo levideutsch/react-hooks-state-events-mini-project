8d3f2e612263e99634fb23e4be9d74e2efd55ef8
levideutsch <levideutsch@live.com>
2022-12-22 18:01:49 -0500

    commit tree 7cbbc8cb85358ef9de7c34d297566a3b8c09f48d

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
 
+
 import { CATEGORIES, TASKS } from "../data";
-console.log("Here's the data you're working with");
-console.log({ CATEGORIES, TASKS });
+// console.log("Here's the data you're working with");
+// console.log({ CATEGORIES, TASKS });
 
 function App() {
+
+  const [tasks, setTasks] = useState(TASKS)
+  const [selectedCategory, setSelectedCategory] = useState("All")
+
+  const [newTask, setNewTask] = useState(TASKS)
+
+  function handleNewTaskAdd(object) {
+    setNewTask([...newTask, object])
+  }
+
+  function handleCategoryChange(event) {
+    console.log(event.target.textContent)
+    setSelectedCategory(event.target.textContent)
+  }
+  
+
+  const categoryToDisplay = newTask.filter((task) => {
+    if (selectedCategory === "All") return true 
+
+    return task.category === selectedCategory
+  })
+
+ 
+
+  const newCategories = [...CATEGORIES]
+  newCategories.shift()
+
+  // console.log(newCategories)
+  // console.log(CATEGORIES)
+
+
+
+
+
   return (
     <div className="App">
-      <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+    <h2>My tasks</h2>
+
+      
+      <CategoryFilter categories={CATEGORIES} onCategoryClickChange={handleCategoryChange} selectedCategory={selectedCategory}/>
+     
+      
+      <NewTaskForm newCategories={newCategories} onTaskFormSubmit={handleNewTaskAdd}/>
+      <TaskList categoryToDisplay={categoryToDisplay} setTasks={setNewTask}/>
+
     </div>
   );
 }
